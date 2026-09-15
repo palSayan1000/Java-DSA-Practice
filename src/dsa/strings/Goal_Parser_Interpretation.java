97239a9223f138b2e97dbcea7f272bb1ecc6bef3
Sayan Pal <palsayan1000@gmail.com>
2026-09-15 15:05:07 +0530

    updated dsa

---
+++ src/dsa/strings/Goal_Parser_Interpretation.java
@@ -0,0 +1,11 @@
+package dsa.strings;
+
+// https://leetcode.com/problems/goal-parser-interpretation/description/
+public class Goal_Parser_Interpretation {
+    static void main() {
+        System.out.println(new Goal_Parser_Interpretation().interpret("G()(al)"));
+    }
+    public String interpret(String command) {
+        return command.replace("()", "o").replace("(al)", "al");
+    }
+}
